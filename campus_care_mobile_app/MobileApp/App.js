1405ad363af9540145917d32f0c90c929e8ef3e0
AlokHalder <34887929+AlokHalder@users.noreply.github.com>
2021-11-14 19:06:22 +0530

    Initial Commit

---
+++ campus_care_mobile_app/MobileApp/App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+  import * as React from 'react';
+  import Login from "./Components/Login";
+  import Dashboard from "./Components/DashBoard";
+  import {
+    StyleSheet
+  } from 'react-native';
+
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+ const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    // <SafeAreaView style={styles.container}>
+    //   <ScrollView style={styles.scrollView}>
+    // <DeContext.Provider>
+   
+    // <Login/>
+       <NavigationContainer>
+         
+        { <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+          />
+          
+          <Stack.Screen
+            name="Dashboard"
+            component={Dashboard}
+          />
+        </Stack.Navigator> }
+      </NavigationContainer> 
+   
+    // </DeContext.Provider>
+    //   </ScrollView>
+    // </SafeAreaView>
+  );
+
+};
+
+
+
+export default App;
---
+++ campus_care_mobile_app/MobileApp/Context/DefaultContext.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const DeContext = React.createContext();

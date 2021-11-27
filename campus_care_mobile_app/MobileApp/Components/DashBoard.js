3cee8eb8d85a8ed3322fdd291e520a149dd6f2ca
AlokHalder <34887929+AlokHalder@users.noreply.github.com>
2021-11-27 18:09:40 +0530

    dashboard menu

--- campus_care_mobile_app/MobileApp/Components/DashBoard.js
+++ campus_care_mobile_app/MobileApp/Components/DashBoard.js
@@ -15,8 +15,31 @@ function Root() {
         headerTitleStyle: {fontWeight: 'bold'},
       }}>
       <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Drawer.Screen name="My Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Aboute Us" component={ProfileScreen} />
+      <Stack.Screen name="Student" component={SettingsScreen} />
+      {/* student dashboard */}
+      <Stack.Screen name="Attendance" component={SettingsScreen} />
+       {/* View of marks */}
+       <Stack.Screen name="SemesterMarks" component={SettingsScreen} /> 
+       {/* Application Status */}
+       <Stack.Screen name="Application" component={SettingsScreen} /> 
+
+       <Stack.Screen name="Course Curriculum" component={SettingsScreen} /> 
+
+       <Stack.Screen name="Class Time Table" component={SettingsScreen} />
+        {/* Training Module */}
+       <Stack.Screen name="Practical Training Schedule" component={SettingsScreen} /> 
+       <Stack.Screen name="Training Feedback" component={SettingsScreen} /> 
+       <Stack.Screen name="Placement Notification" component={SettingsScreen} /> 
+       <Stack.Screen name="Apprentice Training" component={SettingsScreen} /> 
+       <Stack.Screen name="InternShip" component={SettingsScreen} /> 
+       <Stack.Screen name="complain/Grievance" component={SettingsScreen} /> 
+      <Stack.Screen name="Facalties" component={SettingsScreen} />
+      <Stack.Screen name="Announcement" component={SettingsScreen} />
+      <Stack.Screen name="Downloads" component={SettingsScreen} />
+      <Stack.Screen name="Contacts" component={SettingsScreen} />
+      <Stack.Screen name="Logout" component={SettingsScreen} />
     </Drawer.Navigator>
   );
 }
